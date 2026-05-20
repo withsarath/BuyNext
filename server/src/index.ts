4dbb92e9b631fd143b2474df1b584ec3752e5116
withsarath <withsarath.m@gmail.com>
2026-05-20 23:58:08 +0530

    feat: implement user, product, and comment routes and controllers

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,9 @@ import express from "express";
 import { ENV } from "./config/env";
 import {clerkMiddleware} from "@clerk/express"
 import cors from "cors"
+import userRoute from "./routes/userRoute";
+import productRoute from "./routes/productRoute";
+import commentRoute from "./routes/commentRoute";
 
 const app = express();
 
@@ -22,6 +25,9 @@ app.get("/", (req, res) => {
         }
     })
 });
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/comments", commentRoute)
 app.listen(ENV.PORT, () => {
   console.log(`Server is running on: ${ENV.PORT}`);
 });
