77e7ca754d5cc47619429677e157e7ecc765a3f5
withsarath <withsarath.m@gmail.com>
2026-05-22 13:39:18 +0530

    bux fix in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar";
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import ProfilePage from "./pages/ProfilePage";
